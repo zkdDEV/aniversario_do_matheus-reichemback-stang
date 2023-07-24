63685b336dc5b92dbb45dc721a98d1121240dd53
Matheus Reichemback Stang <matheusstang15@gmail.com>
2023-07-24 20:02:31 -0300

    Primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify');
+const imagemin = require("gulp-imagemin");
+const htmlmin = require('gulp-htmlmin');
+
+function html()
+{
+    return gulp.src('./src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist/'))
+}
+function scripts()
+{
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts/'))
+}
+function styles()
+{
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({outputStyle:"compressed"}))
+        .pipe(gulp.dest("./dist/styles/"))
+}
+function images()
+{
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/img"))
+}
+
+
+exports.default = gulp.parallel(html, styles, images, scripts)
+exports.watch = function()
+{
+    gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+confetti.start()
+setTimeout(function(){confetti.stop();},5000)
+
+const dataDoAniversario = new Date('May 03, 2024 00:00:00')
+const timeStampDoAniversario = dataDoAniversario.getTime()
+
+const contaAsHoras = setInterval(function()
+{
+    const agora = new Date()
+    const timeStampAgora = agora.getTime()
+    const tempoQueFalta = timeStampDoAniversario - timeStampAgora
+    
+    const diasAteOAniversario = Math.floor(tempoQueFalta / (1000 * 60 * 60 * 24))
+    const horasAteOAniversario = Math.floor((tempoQueFalta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutosAteOAniversario = Math.floor((tempoQueFalta % (1000 * 60 * 60)) / (1000 * 60))
+    const segundosAteOAniversario = Math.floor((tempoQueFalta % (1000 * 60)) / 1000)
+
+    document.getElementById('tempo').innerHTML = `${diasAteOAniversario}d ${horasAteOAniversario}h ${minutosAteOAniversario}m ${segundosAteOAniversario}s`
+
+    if(tempoQueFalta <= 0)
+    {
+        const idade = 19
+        document.getElementById('tempo').innerHTML = `Agora Matheus tem ${idade} anos`
+        confetti.start()
+        setTimeout(function(){confetti.stop();},10000)
+    }
+}, 1000)
